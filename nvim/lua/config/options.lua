ee3f116250ff44c8d1493defa6142612a3953229
vbaswin <aswinvb10a@gmail.com>
2025-12-31 19:29:17 +0530

    everything is working fine

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- ~/.config/nvim/lua/user/options.lua
+
+local o = vim.opt
+
+o.tabstop = 4 -- a real tab is 4 columns wide
+o.shiftwidth = 4 -- indentation level for >> and autoindent
+o.softtabstop = 4 -- editing: insert/delete behaves like 4
+o.expandtab = false -- USE TABS (do not convert tabs to spaces)
