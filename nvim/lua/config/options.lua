e2130333a2144b911ea6c12ab5e28c73b84e82e7
vbaswin <aswinvb10a@gmail.com>
2026-01-12 02:14:17 +0530

    fix(formatting): use .clang-format for all the formatting

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,6 +8,6 @@ local o = vim.opt
 o.tabstop = 4 -- a real tab is 4 columns wide
 o.shiftwidth = 4 -- indentation level for >> and autoindent
 o.softtabstop = 4 -- editing: insert/delete behaves like 4
-o.expandtab = false -- USE TABS (do not convert tabs to spaces)
+o.expandtab = true -- USE TABS (do not convert tabs to spaces)
 o.wrap = false
 o.textwidth = 0
